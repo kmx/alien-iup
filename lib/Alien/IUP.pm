696ebe0a0b47bab6eda0a17089cfd7663ab96a8e
kmx <kmx@cpan.org>
2010-09-15 10:30:23 +0200

    v0.0.14

--- lib/Alien/IUP.pm
+++ lib/Alien/IUP.pm
@@ -13,11 +13,11 @@ Alien::IUP - Building, finding and using iup + related libraries - L<http://www.
 
 =cut
 
-our $VERSION = 'v0.0.13';
+our $VERSION = 'v0.0.14';
 
 =head1 VERSION
 
-Version v0.0.13 of Alien::IUP is based on the following:
+Version v0.0.14 of Alien::IUP is based on the following:
 
 =over
 
