31f6ba46ed5b8344689de47fa5db0e9d82d0b6da
kmx <kmx@cpan.org>
2010-11-22 17:11:59 +0100

    sorting out diffs

--- _internals/create-diffs.sh
+++ _internals/create-diffs.sh
@@ -1,11 +1,11 @@
-diff -ru im.orig im | grep -v \^Only  > _im.diff
-diff -ru cd.orig cd | grep -v \^Only > _cd.diff
-diff -ru iup.orig iup | grep -v \^Only > _iup.diff
+diff -ru --strip-trailing-cr im.orig im | grep -v \^Only  > _im.diff
+diff -ru --strip-trailing-cr cd.orig cd | grep -v \^Only > _cd.diff
+diff -ru --strip-trailing-cr iup.orig iup | grep -v \^Only > _iup.diff
 
-diff -ru --strip-trailing-cr im.orig im | grep -v \^Only  > _imyy.diff
-diff -ru --strip-trailing-cr cd.orig cd | grep -v \^Only > _cdyy.diff
-diff -ru --strip-trailing-cr iup.orig iup | grep -v \^Only > _iupyy.diff
+#diff -ru im.orig im | grep -v \^Only | sed "s,[\r\n],\n,g" > _imxx.diff
+#diff -ru cd.orig cd | grep -v \^Only | sed "s,[\r\n],\n,g" > _cdxx.diff
+#diff -ru iup.orig iup | grep -v \^Only | sed "s,[\r\n],\n,g" > _iupxx.diff
 
-diff -ru im.orig im | grep -v \^Only | sed "s,[\r\n],\n,g" > _imxx.diff
-diff -ru cd.orig cd | grep -v \^Only | sed "s,[\r\n],\n,g" > _cdxx.diff
-diff -ru iup.orig iup | grep -v \^Only | sed "s,[\r\n],\n,g" > _iupxx.diff
+#diff -ru im.orig im | grep -v \^Only  > _imyy.diff
+#diff -ru cd.orig cd | grep -v \^Only > _cdyy.diff
+#diff -ru iup.orig iup | grep -v \^Only > _iupyy.diff
