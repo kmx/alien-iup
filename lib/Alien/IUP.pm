3c809161bde2ae7649f433238da7ba09ef68e19c
kmx <kmx@cpan.org>
2011-05-12 03:00:39 +0200

    v0.301

--- lib/Alien/IUP.pm
+++ lib/Alien/IUP.pm
@@ -14,12 +14,12 @@ Alien::IUP - Building, finding and using iup + related libraries - L<http://www.
 =cut
 
 # following recommendation from http://www.dagolden.com/index.php/369/version-numbers-should-be-boring/
-our $VERSION = "0.300_01";
+our $VERSION = "0.301";
 $VERSION = eval $VERSION;
 
 =head1 VERSION
 
-Version 0.300_01 of Alien::IUP is based on the following:
+Version 0.301 of Alien::IUP is based on the following:
 
 =over
 
