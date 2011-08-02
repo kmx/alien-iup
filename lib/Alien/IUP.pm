884c849baee6dbe311f26ded7919985bfe21da07
kmx <kmx@cpan.org>
2011-08-02 08:48:41 +0200

    v0.302_3

--- lib/Alien/IUP.pm
+++ lib/Alien/IUP.pm
@@ -14,7 +14,7 @@ Alien::IUP - Building, finding and using iup + related libraries - L<http://www.
 =cut
 
 # following recommendation from http://www.dagolden.com/index.php/369/version-numbers-should-be-boring/
-our $VERSION = "0.302_2";
+our $VERSION = "0.302_3";
 $VERSION = eval $VERSION;
 
 =head1 VERSION
