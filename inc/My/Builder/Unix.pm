329204fb21b1a0e0c3224510eb891364bf874f52
kmx <kmx@cpan.org>
2015-02-04 17:19:29 +0100

    try to add iupmatrixex again

--- inc/My/Builder/Unix.pm
+++ inc/My/Builder/Unix.pm
@@ -158,8 +158,6 @@ sub build_binaries {
     @imtargets  = qw[im im_process im_jp2 im_fftw im_capture];
     @cdtargets  = qw[cd_zlib cd_freetype cd_ftgl cd cd_pdflib cdpdf cdgl]; #xxx add cdcontextplus
     @iuptargets = qw[iup iupcd iupcontrols iupmatrixex iup_pplot iup_mglplot iupgl iupglcontrols iup_scintilla iupim iupimglib iupweb iuptuio iup_plot];
-
-    @iuptargets = grep { $_ !~ /^iupmatrixex$/ } @iuptargets;
   }
   else {
     @imtargets  = qw[im];
@@ -173,7 +171,6 @@ sub build_binaries {
     #  warn "###WARN### skipping iuptuio on Solaris";
     #  @iuptargets = grep { $_ !~ /^iuptuio$/ } @iuptargets;
     #}
-    @iuptargets = grep { $_ !~ /^iupmatrixex$/ } @iuptargets;
 
     if ($^O eq 'solaris') {
       warn "###WARN### skipping iup_mglplot on Solaris (fails to compile)";
